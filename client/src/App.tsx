1080803a2e6b5587615c34d4b21b7f7e8a931fc5
ionzuu <jonathan.costilla@protonmail.com>
2026-08-24 02:10:36 -0500

    Add desing and start with menu

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,31 @@
-import { useEffect, useState } from 'react'
-import { socket } from './services/socket';
+// import { useEffect, useState } from 'react'
+// import { socket } from './services/socket';
+import './assets/styles/app.css'
+import RoleSelection from './components/roleSelection';
 
 function App() {
-  const [isConnected, setConnected] = useState(socket.connected);
-  useEffect(() => {
-    function onConnect(){
-      setConnected(1)
-    }
-    function onDisconnect(){
-      setConnected(0);
-    }
+  // const [isConnected, setConnected] = useState(socket.connected);
+  // useEffect(() => {
+  //   function onConnect(){
+  //     setConnected(1)
+  //   }
+  //   function onDisconnect(){
+  //     setConnected(0);
+  //   }
 
-    socket.on('connect', onConnect);
-    socket.on('disconnect', onDisconnect);
+  //   socket.on('connect', onConnect);
+  //   socket.on('disconnect', onDisconnect);
 
-    return () => {
-      socket.off('connect', onConnect);
-      socket.off('disconnect', onDisconnect);
-    };
-  }, []);
+  //   return () => {
+  //     socket.off('connect', onConnect);
+  //     socket.off('disconnect', onDisconnect);
+  //   };
+  // }, []);
+
   return(
     <>
-    <div>
-      <h1>KDS Grill</h1>
-      <p>
-        Server Status: {' '}
-        <strong style={{color: isConnected ? 'green' : 'red'}}>
-          {isConnected ? 'Connected' :  'Disconnected'}
-        </strong>
-      </p>
+    <div className="MenuSelectormodal">
+      <RoleSelection></RoleSelection>
     </div>
     </>
   )
---
+++ client/src/components/roleSelection.tsx
@@ -0,0 +1,13 @@
+
+export default function roleSelection () {
+    return(
+        <div className="Modalrole">
+            <div className="top-button">
+                <button>Order taker</button>
+            </div>
+            <div className="bottom-button">
+                <button>KDS</button>
+            </div>
+        </div>
+    )
+}
