8505b6bdd7abb27a0b068a5083365ea0a411299c
Jonathan Costilla <99742932+ionzuu@users.noreply.github.com>
2026-09-01 01:00:19 -0500

    .

--- client/src/src/components/POS.tsx
+++ client/src/src/components/POS.tsx
@@ -1,27 +1,97 @@
 import { useState } from "react"
+import { socket } from "../services/socket";
 
 interface Order {
-  id: number;
-  items: string[]
-  total: number
+  id: number,
+  items: string[],
+  total: number,
+  pattiesT: number,
+}
+
+interface Product {
+  name: string,
+  shortcut: string,
+  price: number,
+  patties: number,
 }
 
 export default function POS() {
 
-  const [order, setOrder] = useState<Order>({ id: 0, items: [], total: 0 })
+  const [order, setOrder] = useState<Order>({ id: 0, items: [], total: 0 , pattiesT: 0})
 
-  const handleOrder = (item: string, price: number) => {
+  const product: Product[] = [
+    {
+      name: 'Single SteakBurger',
+      shortcut: 'SingleSteak',
+      price: 5,
+      patties: 1,
+    },
+    {
+      name: 'Double SteakBurger',
+      shortcut: 'DoubleSteak',
+      price: 7.5,
+      patties: 2,
+    },
+    {
+      name: 'Triple SteakBurger',
+      shortcut: 'TipleSteak',
+      price: 10,
+      patties: 3,
+    }
+  ]
+
+  const onhandleSubmit = () => {
+    if(order.id != 0){
+      setOrder({
+        ...order,
+        id: order.id+1,
+      })
+    }else{
+      setOrder({
+        ...order,
+        id: 0,
+      })
+      setOrder({...order, id: order.id+1})
+    }
+    const newOrder = { ...order ,
+      id: crypto.randomUUID(),
+      orderNum: order.id,
+      createdAt: new Date().toISOString(),
+      item:[
+        order,
+      ]
+    }
+    console.log('Order:', newOrder);
+  }
+
+  const handleOrder = (item: string) => {
+    const newItem:Product = product.find(pr => pr.name === item)
     setOrder({
       ...order,
-      id: order.id + 1,
       items: [...order.items, item],
-      total: order.total + price,
+      total: order.total + newItem?.price,
+      pattiesT: order.pattiesT + newItem.patties,
     })
-    console.log(order);
+    console.log('Item: ', newItem)
+    console.log('Order:', order);
   }
 
   const onHandleDelete = (index: number) => {
     console.log(index);
+    let itemSelected:Product | undefined;
+    let newOrder: Order;
+    let newTotal: number;
+    itemSelected = product.find((id) => id.name === order.items[index])
+    newOrder = {...order}
+    newOrder.items.splice(index, 1);
+    newTotal = order.total - itemSelected.price;
+    console.log('newTotal: ', newTotal)
+    console.log('Price: ', itemSelected?.price)
+    setOrder({
+      ...order,
+      items: newOrder.items,
+      total: newTotal,
+    })
   }
 
   return (
@@ -30,18 +100,18 @@ export default function POS() {
         <div className="grid-item">
           <div className="Receipt">
             <div className="grid-receipt">
-              <div className="grid-item">Order #___</div>
+              <div className="grid-item">Order #{order.id}</div>
               <div className="grid-item list">
                 <h4>Order details</h4>
                 {order.items.map((item, index) => (
-                  <div className="order-details">
-                    <div className="order-item" key={index}>{item}</div>
-                    <div className="deleteItem" onClick={() => onHandleDelete(index)} >X</div>
+                  <div className="order-details" key={index}>
+                    <div className="order-item">{item}</div>
+                    <div className="deleteItem" onClick={() => onHandleDelete(index)} >-</div>
                   </div>
                 ))}
               </div>
-              <div className="grid-item">Total: ${order.total} </div>
-              <div className="grid-item"><button>Place Order</button></div>
+              <div className="grid-item total">Total: ${order.total} </div>
+              <div className="grid-item placeO"><button onClick={() => onhandleSubmit()}>Place Order</button></div>
             </div>
           </div>
         </div>
@@ -50,8 +120,11 @@ export default function POS() {
             <div className="grid-menu">
               <div className="grid-item">
                 <div className="MenuItem">
-                  <h2 className="item-POS" onClick={() => handleOrder('Single SteakBurger', 5)}>Single SteakBurger</h2>
-                  <h2 className="item-POS" onClick={() => handleOrder('Double SteakBurger', 10)}>Double SteakBurger</h2>
+                {product.map((products, index) => (
+                  <h2 className="item-POS" key={index} onClick={() => handleOrder(products.name)}>{products.name}</h2>
+                ))
+
+                }  
                 </div>
               </div>
             </div>
